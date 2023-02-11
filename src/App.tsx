cd55116b576104facb15ab25a8e130378347cd5a
Krushang Kasundra <kruzkasu223@gmail.com>
2023-02-11 14:59:50 +0530

    chore: minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "./assets/vite.svg"
 import { DOMMessage, DOMMessageResponse } from "./types"
 
 function App() {
